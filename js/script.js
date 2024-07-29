739a7013c845491ff83c222f407f7c2bafb03fef
maya-taylor <maya2017taylor@gmail.com>
2024-07-29 16:02:41 -0700

    group id issue

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const FLICKR_API_KEY = '15786247322ab080d4926abdc12b1e40';
-const GROUP_ID = '1551955@N23'; // Group ID for "Field Guide: Birds of the World"
+const GROUP_ID = '42637302@N00'; // Group ID for "Field Guide: Birds of the World"
 let birdData = [];
 
 // Fetch bird data from Flickr
